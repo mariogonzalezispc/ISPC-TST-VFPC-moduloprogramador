0f1524de8349322393f7264a0a1ac5f4355f6cb2
mario <mario.gonzalez@hotmail.com.ar>
2022-08-10 21:49:06 -0300

    sql

--- script.sql
+++ script.sql
@@ -37,7 +37,7 @@ CREATE TABLE `TST_BD_VFPC`.`Historial_compras` (
 /*-------------------------------------------------------*/
 CREATE TABLE `TST_BD_VFPC`.`Cliente` (
     `DNI` INT NOT NULL,
-    `Nro_cliente` INT NOT NULL,
+    `Nro_cliente` INT NOT NULL AUTO_INCREMENT,
     `Apellido_paterno` VARCHAR(45) NULL,
     `Apellido_materno` VARCHAR(45) NULL,
     `Nombre` VARCHAR(45) NULL,
@@ -48,10 +48,48 @@ CREATE TABLE `TST_BD_VFPC`.`Cliente` (
     `Telefono_cel2` VARCHAR(45) NULL,
     `Edad` INT NULL,
     `Email` VARCHAR(45) NULL,
-    `fechaAlta` DATE NULL,
+    `Fecha_Alta` DATE NULL,
     `id_carrito` INT NULL,
     `id_istorial` INT NULL,
     PRIMARY KEY (`DNI`)
     FOREIGN KEY (`id_carrito`) REFERENCES `Carrito_compras`(`id_carrito`),
-    FOREIGN KEY (`id_istorial`) REFERENCES `Historial_compras`(`id_historial`))
-ENGINE = InnoDB;
+    FOREIGN KEY (`id_historial`) REFERENCES `Historial_compras`(`id_historial`))
+     ENGINE = InnoDB;
+/*-------------------------------------------------------*/
+/*Paso 06 carga clientes                                 */
+/*-------------------------------------------------------*/
+INSERT INTO `Cliente` (
+        `DNI`,
+        `Nro_cliente`,
+        `Apellido_paterno`,
+        `Apellido_materno`,
+        `Nombre`,
+        `Calle`,
+        `Numero`,
+        `Barrio`,
+        `Telefono_cel1`,
+        `Telefono_cel2`,
+        `Edad`,
+        `Email`,
+        `Fecha_Alta`,
+        `id_carrito`,
+        `id_historial`
+        )
+VALUES (
+        '24569781',
+        'NULL',
+        'Gonzalez',
+        'Fornaro',
+        'Juan',
+        'Pueyrredon',
+        '123',
+        'Nueva Cordoba',
+        '3518123456',
+        '3512456789',
+        '20',
+        'juan.g@gmail.com',
+        '01-08-2018',
+        'NULL',
+        'NULL'
+    );
+/*-------------------------------------------------------*/
