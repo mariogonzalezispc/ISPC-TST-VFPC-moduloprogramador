f9a921d03501cdfc90d89d8fcbc8f5621495b1b0
Mario <mario.gonzalez@hotmail.com.ar>
2022-08-12 19:36:00 -0300

    bs

--- Base.sql
+++ Base.sql
@@ -1,13 +1,15 @@
+
 /*-------------------------------------------------------*/
 /*Paso 01 Crea  base de datos para el trabajo practico   */
 /*-------------------------------------------------------*/
 CREATE DATABASE TST_BD_VFPC;
+
 /*-------------------------------------------------------*/
 /*Paso 02 Seleciono la base de datos del TST             */
 /*-------------------------------------------------------*/
 USE TST_BD_VFPC;
 
 /*-------------------------------------------------------*/
-/*Paso 02 Seleciono la base de datos del TST             */
+/*Paso 03 Comando elimina base de datos              */
 /*-------------------------------------------------------*/
 DROP DATABASE TST_BD_VFPC;
